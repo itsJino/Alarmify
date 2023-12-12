75f6e1ee50c57462268366d84755fadf4f360382
joshuaflores <floresjoshua2603@gmail.com>
2023-12-12 06:28:22 +0000

    Create ReminderReceiver

---
+++ app/src/main/java/com/example/alarmify/ReminderReceiver.java
@@ -0,0 +1,32 @@
+package com.example.alarmify;
+
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class ReminderReceiver extends BroadcastReceiver {
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent nextActivity = new Intent(context, NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, nextActivity, PendingIntent.FLAG_MUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "alarmifyReminder")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Alarmify")
+                .setContentText("GET MOVING!")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(123, builder.build());
+    }
+}
